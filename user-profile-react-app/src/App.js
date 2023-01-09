0eca98af2340b1b606791a5236ad15cef9e6287f
SushmaGowda7 <110340352+SushmaGowda7@users.noreply.github.com>
2023-01-09 23:59:34 +0530

    user profile is added

--- user-profile-react-app/src/App.js
+++ user-profile-react-app/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { Fragment, useEffect, useState } from "react";
+import Profile from "./components/Profile";
 
-function App() {
+
+const App = () => {
+  const [profileDetails, setProfileDetails] = useState([]);
+
+  const fetchingData = async () => {
+    try {
+      const response = await axios.get("db/profile.json");
+      setProfileDetails(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Profile profileDetails={profileDetails} />
+    </Fragment>
   );
 }
 
